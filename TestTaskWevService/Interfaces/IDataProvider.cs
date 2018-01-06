71f20e102e53fc68e6eeab4bab0b625ad0505fbf
asaprykin@it.ru <asaprykin@it.ru>
2018-01-06 04:07:53 +0300

    Добавлены основные интерфейсы

---
+++ TestTaskWevService/Interfaces/IDataProvider.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using TestTaskWevService.Models.Entities;
+
+namespace TestTaskWevService.Interfaces
+{
+    public interface IDataProvider
+    {
+        /// <summary>
+        /// Возвращаеи всех пользователей, соответсвующих условию
+        /// </summary>
+        /// <param name="condition">Выраженеи, содержащее делегат, содержащий условие отбора</param>
+        /// <returns>Список пользователей</returns>
+        IEnumerable<User> GetUsers(Expression<Func<User, bool>> condition);
+        /// <summary>
+        /// Возвращаеи все альбомы, соответсвующих условию
+        /// </summary>
+        /// <param name="condition">Выраженеи, содержащее делегат, содержащий условие отбора</param>
+        /// <returns></returns>
+        IEnumerable<Album> GetAlbums(Expression<Func<Album, bool>> condition);
+    }
+}
---
+++ TestTaskWevService/Interfaces/IMainService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestTaskWevService.Models.ApiModels;
+
+namespace TestTaskWevService.Interfaces
+{
+    /// <summary>
+    /// Интерфейс, описывающий основные методы сервиса.    
+    /// </summary>
+    public interface IMainService
+    {
+        /// <summary>
+        /// Возвращает список всех пользователей c зашифрованным email
+        /// </summary>
+        /// <param name="publicRsaKey">Открытая часть ключа широфания RSA</param>
+        /// <returns></returns>
+        IEnumerable<UserApi> GetAllUsers(string publicRsaKey);
+        /// <summary>
+        /// Возвращает данные пользователя по его идентификатору
+        /// </summary>
+        /// <param name="userId">Идентификатор пользователя</param>
+        /// <param name="publicRsaKey">Открытая часть ключа широфания RSA</param>
+        /// <returns></returns>
+        UserApi GetUserById(int userId, string publicRsaKey);
+        /// <summary>
+        /// Возвращает список всех альбомов
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<AlbumApi> GetAllAlbums();
+        /// <summary>
+        /// Возвращает список всех альбомов одного пользователя c зашифрованным email
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="publicRsaKey">Открытая часть ключа широфания RSA</param>
+        /// <returns></returns>
+        IEnumerable<UserAlbumApi> GetAllAlbumsForUser(int userId, string publicRsaKey);
+        /// <summary>
+        /// Возвращает иалюбом по его идентификатору
+        /// </summary>
+        /// <param name="albumId">Идентификатор альбома</param>
+        /// <returns></returns>
+        AlbumApi GetAlbumById(int albumId);
+    }
+}
