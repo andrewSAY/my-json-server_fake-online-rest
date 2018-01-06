ba8cefe8423d252d180e8d7225d769a4344df2eb
asaprykin@it.ru <asaprykin@it.ru>
2018-01-06 07:46:41 +0300

    Реализован поставщик данных, берущий данные с http://jsonplaceholder.typicode.com/.

---
+++ TestTaskWevService.Tests/TestClasses/RestDataProviderTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestTaskWevService.Providers;
+using NUnit.Framework;
+
+namespace TestTaskWevService.Tests.TestClasses
+{   [TestFixture]
+    public class RestDataProviderTests
+    {
+        #region preparing area
+        private RestDataProvider GetProviderUnderTests()
+        {
+            return new RestDataProvider();
+        }
+        #endregion
+        [Test]
+        public void GetAlbumsAsync_WhereIdEquals7_ReturnsAlbumWithId7()
+        {
+            var provider = GetProviderUnderTests();
+            var album = provider.GetAlbumsAsync(a => a.Id == 7).Result.FirstOrDefault();
+
+            Assert.AreEqual(7, album.Id);
+        }
+        [Test]
+        public void GetAlbumsAsync_WhereUserUserIdEquals7_Returns10Albums()
+        {
+            var provider = GetProviderUnderTests();
+            var albums = provider.GetAlbumsAsync(a => a.UserId == 7).Result;
+
+            Assert.AreEqual(10, albums.Count());
+        }
+        [Test]
+        public void GetAlbumsAsync_WhereTrue_Returns100Albums()
+        {
+            var provider = GetProviderUnderTests();
+            var albums = provider.GetAlbumsAsync(a => true).Result;
+
+            Assert.AreEqual(100, albums.Count());
+        }
+        [Test]
+        public void GetUsersAsync_WhereIdEquals5_ReturnsUserWihtId5()
+        {
+            var provider = GetProviderUnderTests();
+            var user = provider.GetUsersAsync(a => a.Id == 5).Result.FirstOrDefault();
+
+            Assert.AreEqual(5, user.Id);
+        }
+        [Test]
+        public void GetUsersAsync_WhereTrue_Returns10Users()
+        {
+            var provider = GetProviderUnderTests();
+            var users = provider.GetUsersAsync(a => true).Result;
+
+            Assert.AreEqual(10, users.Count());
+        }
+
+    }
+}
---
+++ TestTaskWevService/Providers/RestDataProvider.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using System.Net.Http;
+using TestTaskWevService.Interfaces;
+using TestTaskWevService.Models.Entities;
+using Newtonsoft.Json;
+using static System.String;
+
+namespace TestTaskWevService.Providers
+{
+    public class RestDataProvider : IDataProvider
+    {
+        protected string RestUrl { get; set; }
+        public RestDataProvider()
+        {
+            RestUrl = "http://jsonplaceholder.typicode.com";
+        }       
+        public async Task<IEnumerable<Album>> GetAlbumsAsync(Expression<Func<Album, bool>> condition)
+        {
+            return await GetDataFromRemoteServer(condition);
+        }
+
+        public async Task<IEnumerable<User>> GetUsersAsync(Expression<Func<User, bool>> condition)
+        {
+            return await GetDataFromRemoteServer(condition);
+        }
+
+        private async Task<IEnumerable<T>> GetDataFromRemoteServer<T>(Expression<Func<T, bool>> condition)
+        {
+            var client = new HttpClient();
+            var uri = new Uri($"{GetUrlFromExpression(condition)}");
+            var response = await client.GetStringAsync(uri);
+            var items = JsonConvert.DeserializeObject<IEnumerable<T>>(response);
+            return items;
+        }
+
+        private string GetUrlFromExpression<T>(Expression<Func<T, bool>> expression)
+        {
+            var lambdaBody = expression.Body;
+            var entity = typeof(T).Name;
+            if (lambdaBody.NodeType == ExpressionType.Equal)
+                return GetUrlFromEqualExpression(entity, lambdaBody);
+            if (lambdaBody.NodeType == ExpressionType.Constant)
+                return GetUrlFromConstantExpression(entity, lambdaBody);
+            return Empty;
+         }
+        private string GetUrlFromEqualExpression(string entityName, Expression body)
+        {
+            var binaryBody = body as BinaryExpression;
+            var left = binaryBody.Left as MemberExpression;
+            var right = binaryBody.Right as ConstantExpression;
+            var searchFieldName = ToCamelCase(left.Member.Name);
+            var searchFieldValue = right.Value;
+            var rightEntityName = $"{ToCamelCase(entityName)}s";
+
+            var url = $"{RestUrl}/{rightEntityName}?{searchFieldName}={searchFieldValue}";
+
+            return url;
+        }
+        private string GetUrlFromConstantExpression(string entityName, Expression body)
+        {
+            var binaryBody = body as ConstantExpression;
+            var rightEntityName = $"{ToCamelCase(entityName)}s";
+            if (binaryBody.Type == typeof(bool))                
+                return $"{RestUrl}/{rightEntityName}";
+
+            return Empty;
+        }
+        private string ToCamelCase(string value)
+        {
+            var valueCharArray = value.ToCharArray();
+            var fisrtSymbol = valueCharArray[0];
+            valueCharArray[0] = char.ToLowerInvariant(fisrtSymbol);
+            return new String(valueCharArray);
+        }        
+    }
+}
